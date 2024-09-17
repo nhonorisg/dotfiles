84e038fdce244d9cfa52e349746ad86170c54cb2
Honoris Ndereyimana <honoris-gratias.ndereyimana@etu.univ-tlse2.fr>
2024-09-17 10:52:03 +0200

    [i3]: Updating wm config.
    * Changed text font after scaling xorg display view in Xresources.
    * Set screen light auto adjustment using "clight" package

--- Zsh/dot-oh-my-zsh/custom/shortcuts.zsh
+++ Zsh/dot-oh-my-zsh/custom/shortcuts.zsh
@@ -11,6 +11,26 @@ new_ex_file() {
 # file creation aliases
 alias t='touch' 
 alias texfl='new_ex_file'
-alias stowd='stow --dotfiles'
+
+alias lc='ls -a'
+alias q='exit'
 alias nv='nvim'
+alias stowd='stow --dotfiles'
+alias bt='bluetui'
+
+# clipboard utility
+alias clcp='xclip -sel clip'
+alias clp='xclip -o'
+
+# Tui tools
 alias lg='lazygit'
+alias yz='yazi'
+
+# network management
+# this alias shows avalaible network list.
+alias wfnls='nmcli d w'
+# Activating wifi hotspot
+alias wfhp='nmcli d w hotspot ifname wlp0s20f3 ssid M2-ice-ld password ELnP1RX3PW'
+
+# Generating 10 random character
+alias grc="cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1"
