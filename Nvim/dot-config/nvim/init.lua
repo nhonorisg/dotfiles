6ee37101b4067a0c410aa58c9a76b3fb3bc5cc00
Honoris Ndereyimana <nhonorisgratias@gmail.com>
2024-10-01 16:08:05 +0200

    [nvim]: Setting tab to match 4 space character.

--- Nvim/dot-config/nvim/init.lua
+++ Nvim/dot-config/nvim/init.lua
@@ -38,3 +38,7 @@ end)
 
 -- Enabling relative numbers
 vim.wo.relativenumber = true
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
